10ff761443b2172bb0bd56b5ca27e908cf213cc2
VirGem25 <chhetri.adi@gmail.com>
2021-05-06 11:24:08 -0400

    Unity Project

---
+++ Unity Project/05-05_New Unity Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameEnded = false;
+
+    public float restartDelay;
+
+    public void EndGame()
+    {
+        if(gameEnded == false)
+        {
+            Debug.Log("GAME OVER");
+            gameEnded = true;
+            Invoke("Restart", restartDelay);
+        }
+        
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
